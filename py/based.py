fc90b924afc7f0a2bbe8e0a5a0256b0c39e45c89
takuto0831 <takut0.mirage@gmail.com>
2019-07-11 01:38:39 +0900

    rename

--- py/based.py
+++ py/based.py
@@ -31,22 +31,16 @@ class Process:
         file_name = self.home_path + '/output/submit/' + tech + '_' + datetime.now().strftime("%Y%m%d") + ".csv"
         submit_file.to_csv(file_name, index=False)
         
-    def open_parameter(self,file_name):
-        f = open(self.home_path + '/input/parameters/' + file_name + '.txt', 'rb')
-        list_ = pickle.load(f)
-        return list_
-        
-    # 動作未確認 !!!!
     def display_importances(self,importance_df,title,file_name = None):
         cols = (importance_df[["feature", "importance"]]
                 .groupby("feature")
                 .mean()
                 .sort_values(by="importance", ascending=False)[:200].index)
         best_features = importance_df.loc[importance_df.feature.isin(cols)]
-        plt.figure(figsize=(14,50))
+        plt.figure(figsize=(10,20))
         sns.barplot(x="importance",y="feature",
                     data=best_features.sort_values(by="importance",ascending=False))
-        plt.title(title + 'Features (avg over folds)')
+        plt.title(title + ' Features (avg over folds)')
         plt.tight_layout()
         # save or not
         if file_name is not None: 
@@ -72,28 +66,37 @@ class Assistance:
         # setting
         submit_df = pd.DataFrame()
         log_mae = []
+        importance_all = pd.DataFrame()
         # split list
         split_list = train[split_value].unique().copy()
         # model execute by split list
         for list_ in split_list:
             print("~~~~~~~~~ type name:", list_, " ~~~~~~~~~")
             # update model_arg dictionary
-            train_ = train[train[split_value] == list_].copy()
-            test_ = test[test[split_value] == list_].copy()
+            train_ = train[train[split_value] == list_].reset_index(drop=True)
+            test_ = test[test[split_value] == list_].reset_index(drop=True)
             model_arg.update(train = train_, test = test_)
             # model
             val_pred, test_pred, importance = model(**model_arg)
-            # concat data and validation value
-            log_mae.append(np.log(mean_absolute_error(train_[self.target].values, val_pred)))
-            test_[self.target] = test_pred        
+            # summary result
+            importance_all = pd.concat([importance_all, importance], axis=0) # importance
+            log_mae.append(np.log(mean_absolute_error(train_[self.target].values, val_pred))) # validation
+            test_[self.target] = test_pred # test
             submit_df = pd.concat([submit_df,test_[[self.idx,self.target]]], axis=0)
         # sort by id
         submit_df = submit_df.sort_values(self.idx)
-        return log_mae, submit_df
+        # mean of columns
+        return log_mae, submit_df, importance_all
+        
+    def test_sampling(self, train, key, rate=0.2):
+        # type のごとにランダムサンプリング
+        train_ = train.groupby(key).apply(lambda x: x.sample(n=round(len(x) * rate), random_state=831))
+        train__ = train_.reset_index(level=key, drop=True).reset_index(drop=True)
+        return train__
   
 # other 
 def line(text):
-    line_notify_token = '07tI1nvYaAtGaLdsCaxKZxkboOU0OsvLregXqodN2ZV' #先程発行したコードを貼ります
+    line_notify_token = '07tI1nvYaAtGaLdsCaxKZxkboOU0OsvLregXqodN2ZV' # 発行したコード
     line_notify_api = 'https://notify-api.line.me/api/notify'
     message = '\n' + text
     # 変数messageに文字列をいれて送信します トークン名の隣に文字が来てしまうので最初に改行しました
